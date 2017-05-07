2b3099b60a99ceec833c9e2e15f04221b12a1e93
alisher <t.b.alisher@gmail.com>
2017-05-07 17:52:48 +0900

    search works

--- v1/search.js
+++ v1/search.js
@@ -1,2 +1,27 @@
+$(function ()
+{
+    $('#btnSearch').show().click(function () { Search($("#txtSearchTerm").val(),0);});
+    $('#lnkPrev').click(function () { Search($("#txtSearchTerm").val(),-1); });
+    $('#lnkNext').click(function () { Search($("#txtSearchTerm").val(),1);  });
+});
 
+function Search(term, direction)
+{
+	var query = term.split(" ").join("+");
+	console.log(query);
+	var url = "https://www.googleapis.com/customsearch/v1?key="
+    + "AIzaSyCIjld6maNImk6Z2bWQT3HEI1GwIICBuyk" + "&num=10&cx=" + "017406780437479842754:il_s2aky6zc" + "&q=" + query + "&searchType=image&callback=?";
+    // url = "http://localhost/dummy.js?callback=?";
+    $.getJSON(url, '', SearchCompleted);
+}
 
+function SearchCompleted(response)
+{
+    var html = "";
+    for (var i = 0; i < response.items.length; i++) {
+  		var item = response.items[i];
+  		html += '<br>' +  '<img src="' + item.link + '" width="300"/>';
+	}
+	$("#output").html(html);
+}
+// https://peter.hahndorf.eu/blog/UsingTheGoogleCustomSearchAPIV.html
